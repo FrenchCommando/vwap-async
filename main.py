b0d4bb3a9259b4320657ddba78949ab7a4916797
Macfly <cocoval@gmail.com>
2022-01-09 19:00:50 -0500

    use delay

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import asyncio
 import datetime
+
 import aiohttp
 
 
 def print_current():
     now = datetime.datetime.now()
     print(
-        f"{now}\t\ttotal volume\t{current_volume:.8f}\t\t"
-        f"currentvwap\t\t{current_price_times_volume / current_volume if current_volume != 0 else 0:.2f}"
-    )
+        f"{now}\t\ttotal volume {current_volume}\t"
+        f"currentvwap {current_price_times_volume / current_volume if current_volume != 0 else 0}")
 
 
-async def revert(price, volume):
+def revert(price, volume):
     global current_price_times_volume
     global current_volume
-    await asyncio.sleep(time_window_seconds)
     current_price_times_volume -= price * volume
     current_volume -= volume
-    # print("In revert", price, volume)
     print_current()
 
 
@@ -26,13 +24,12 @@ async def process(price, volume):
     global current_volume
     current_price_times_volume += price * volume
     current_volume += volume
-    # print("in process", price, volume)
     print_current()
 
 
 async def process_and_revert(price, volume):
     asyncio.create_task(process(price, volume))
-    asyncio.create_task(revert(price, volume))
+    asyncio.get_event_loop().call_later(time_window_seconds, revert, price, volume)
 
 
 async def main():
@@ -40,9 +37,7 @@ async def main():
     async with session.ws_connect('wss://stream.binance.com:9443/ws/btcusdt@trade') as ws:
         async for msg in ws:
             rep_json = msg.json()
-            asyncio.create_task(process_and_revert(
-                price=float(rep_json['p']), volume=float(rep_json['q'])
-            ))
+            asyncio.create_task(process_and_revert(price=float(rep_json['p']), volume=float(rep_json['q'])))
 
 
 time_window_seconds = 60
