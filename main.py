c1bb4489f18b28e450b5248e890cd79ab9b3d003
FrenchCommando <martialren@gmail.com>
2022-01-09 21:38:16 -0500

    async only async - call_later - testing

--- main.py
+++ main.py
@@ -11,31 +11,30 @@ def print_current():
     )
 
 
-async def revert(price, volume):
+def revert(price, volume):
     global current_price_times_volume
     global current_volume
-    await asyncio.sleep(time_window_seconds)
     current_price_times_volume -= price * volume
     current_volume -= volume
-    # print("In revert", price, volume)
+    # print("\t\t\t\t\t\tIn revert", price, volume)
     print_current()
 
 
-async def process(price, volume):
+def process(price, volume):
     global current_price_times_volume
     global current_volume
     current_price_times_volume += price * volume
     current_volume += volume
-    # print("in process", price, volume)
+    # print("\t\t\t\t\t\tIn process", price, volume)
     print_current()
 
 
 async def process_and_revert(price, volume):
-    asyncio.create_task(process(price, volume))
-    asyncio.create_task(revert(price, volume))
+    process(price, volume)
+    asyncio.get_event_loop().call_later(time_window_seconds, revert, price, volume)
 
 
-async def main():
+async def main_binance():
     session = aiohttp.ClientSession()
     async with session.ws_connect('wss://stream.binance.com:9443/ws/btcusdt@trade') as ws:
         async for msg in ws:
@@ -45,8 +44,20 @@ async def main():
             ))
 
 
-time_window_seconds = 60
-current_price_times_volume = 0
-current_volume = 0
+async def main_short():
+    asyncio.create_task(process_and_revert(price=100, volume=1))
+    await asyncio.sleep(3)
+    asyncio.create_task(process_and_revert(price=200, volume=3))
+    await asyncio.sleep(10)
 
-asyncio.run(main())
+
+if __name__ == '__main__':
+    current_price_times_volume = 0
+    current_volume = 0
+    short_only = False
+    if short_only:
+        time_window_seconds = 5
+        asyncio.run(main_short())
+    else:
+        time_window_seconds = 60
+        asyncio.run(main_binance())
